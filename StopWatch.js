15d8563c718274d4d96015c2c79e7adc4adaef54
Salman Hassan <manujutt1307@gmail.com>
2024-08-28 23:23:05 +0500

    wroong

---
+++ StopWatch.js
@@ -0,0 +1,39 @@
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isrunning = false;
+
+function start(){
+
+    if (!isrunning) {
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isrunning = true;
+    }
+}    
+
+function stop(){
+    
+}
+
+function reset(){
+    
+}
+
+function update(){
+    const currenttime = Date.now();
+    elapsedTime = currenttime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let mnts = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let scnds = Math.floor(elapsedTime / (1000 % 60));
+    let miliscnds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    mnts = String(mnts).padStart(2, "0");
+    scnds = String(scnds).padStart(2, "0");
+    miliscnds = String(miliscnds).padStart(2, "0");
+
+    display.textContent = `${hours}:${mnts}:${scnds}:${miliscnds}`;
+}
